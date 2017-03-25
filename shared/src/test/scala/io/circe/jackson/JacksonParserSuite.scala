3235334690c5a32a7d1cf27d211182c9cee98a21
Lari Hotari <lari.hotari@zalando.fi>
2017-03-25 15:08:22 +0200

    Add failing test when using Jackson's MappingIterator

--- shared/src/test/scala/io/circe/jackson/JacksonParserSuite.scala
+++ shared/src/test/scala/io/circe/jackson/JacksonParserSuite.scala
@@ -1,9 +1,13 @@
 package io.circe.jackson
 
+
 import cats.data.Validated
+import com.fasterxml.jackson.core.JsonToken
+import com.fasterxml.jackson.databind.{ObjectMapper, ObjectReader}
 import io.circe.Json
 import io.circe.testing.ParserTests
-import java.io.File
+import java.io.{ByteArrayInputStream, File}
+
 import scala.io.Source
 
 class JacksonParserSuite extends CirceSuite with JacksonInstances {
@@ -32,14 +36,60 @@ class JacksonParserSuite extends CirceSuite with JacksonInstances {
     assert(parseFile(file) === Right(glossary))
   }
 
-  "parseByteArray and decodeByteArray(Accumulating)" should "parse an array of bytes" in {
-    val stream = getClass.getResourceAsStream("/io/circe/jackson/examples/glossary.json")
-    val source = Source.fromInputStream(stream)
-    val bytes = source.map(_.toByte).toArray
-    source.close()
+  "parseByteArray and decodeByteArray(Accumulating)" should "parse an array of elementAsBytes" in {
+    val bytes = glossaryAsBytes
 
     assert(decodeByteArray[Json](bytes) === Right(glossary))
     assert(decodeByteArrayAccumulating[Json](bytes) === Validated.valid(glossary))
     assert(parseByteArray(bytes) === Right(glossary))
+  }
+
+  for (elementCount <- 1 to 4) {
+    "CirceJsonDeserializer" should s"be useable with Jackson's MappingIterator " +
+      s"with ${elementCount} elements in array" in {
+      val input = new ByteArrayInputStream(createJsonArrayAsBytes(glossaryAsBytes, elementCount))
+      val objectMapper = new ObjectMapper()
+      objectMapper.registerModule(CirceJsonModule)
+      val jsonParser = objectMapper.getFactory.createParser(input)
+
+      assert(jsonParser.nextToken() == JsonToken.START_ARRAY)
+      assert(jsonParser.nextToken() == JsonToken.START_OBJECT)
+
+      val reader = createReader(objectMapper).forType(classOf[Json])
+      val iterator = reader.readValues[Json](jsonParser)
+      var counter = 0
+      while (iterator.hasNext) {
+        val glossaryFromIterator = iterator.next()
+        assert(glossary == glossaryFromIterator)
+        counter = counter + 1
+      }
+      assert(counter == elementCount)
+    }
+  }
+
+  // workaround warnings from compiler with Jackson 2.5
+  @unchecked
+  private def createReader(objectMapper: ObjectMapper): ObjectReader =
+    objectMapper.reader()
+
+  private def createJsonArrayAsBytes(elementAsBytes: Array[Byte], elementCount: Int): Array[Byte] = {
+    val byteArrayOutput = new java.io.ByteArrayOutputStream()
+    byteArrayOutput.write('[')
+    for (i <- 1 to elementCount) {
+      if (i != 1) {
+        byteArrayOutput.write(',')
+      }
+      byteArrayOutput.write(elementAsBytes)
+    }
+    byteArrayOutput.write(']')
+    byteArrayOutput.toByteArray
+  }
+
+  private def glossaryAsBytes = {
+    val stream = getClass.getResourceAsStream("/io/circe/jackson/examples/glossary.json")
+    val source = Source.fromInputStream(stream)
+    val bytes = source.map(_.toByte).toArray
+    source.close()
+    bytes
   }
 }

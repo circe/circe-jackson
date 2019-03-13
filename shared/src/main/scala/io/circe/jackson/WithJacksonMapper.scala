0e31df0dff4107ab47c5526ce8e6d843ff422fc1
Ben Fradet <benjamin.fradet@gmail.com>
2019-03-13 15:03:21 +0100

    Make object mapper public

--- shared/src/main/scala/io/circe/jackson/WithJacksonMapper.scala
+++ shared/src/main/scala/io/circe/jackson/WithJacksonMapper.scala
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import java.io.{ File, Writer }
 
 class WithJacksonMapper {
-  protected final val mapper: ObjectMapper = (new ObjectMapper).registerModule(CirceJsonModule)
+  final val mapper: ObjectMapper = (new ObjectMapper).registerModule(CirceJsonModule)
   private[this] final val jsonFactory: JsonFactory = new JsonFactory(mapper)
 
   protected final def jsonStringParser(input: String): JsonParser = jsonFactory.createParser(input)
